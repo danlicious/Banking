25383a5cd835d22475282f28ea46fc3107b54a78
MariusAxente <57196700+MariusAxente@users.noreply.github.com>
2019-11-22 15:00:15 -0500

    Update SavingsAccount.java

--- Classes/SavingsAccount.java
+++ Classes/SavingsAccount.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package banking;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author cstuser
  */
-public class SavingsAccount {
+public class SavingsAccount extends Account{
     
+    public SavingsAccount(int accountNumber, double balance, ArrayList<Transaction> transactions, Client owner, String type) {
+        super(accountNumber, balance, transactions, owner, type = "Savings");
+    }
+
+    public SavingsAccount() {
+    }
+
+    
+    
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(ArrayList<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public Client getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
 }
