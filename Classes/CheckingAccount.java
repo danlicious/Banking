c23abc8ff7099d44b748f614b3c7b35a3ffac8bb
MariusAxente <57196700+MariusAxente@users.noreply.github.com>
2019-11-22 15:01:02 -0500

    Update CheckingAccount.java

--- Classes/CheckingAccount.java
+++ Classes/CheckingAccount.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package banking;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author cstuser
  */
-public class CheckingAccount {
+public class CheckingAccount extends Account{
+
+    public CheckingAccount(int accountNumber, double balance, ArrayList<Transaction> transactions, Client owner, String type) {
+        super(accountNumber, balance, transactions, owner, type = "Checking");
+    }
+
+    public CheckingAccount() {
+    }
+
+    
+    
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(ArrayList<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public Client getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
     
 }
