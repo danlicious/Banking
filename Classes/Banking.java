9caaae323f356e0cc3f312ab0123692edec7f3f1
Dan Petr <55291722+danlicious@users.noreply.github.com>
2019-11-23 11:12:53 -0500

    UserInputManager: Fixed potential crashes
    When userOption is not an int is a number thats not in (1,7), it asks for a second input until its the right option.
    
    Did not have time to implement the functionality into string checks and double checks, but that will be easy as the basic framework for it is already in the program.

--- Classes/Banking.java
+++ Classes/Banking.java
@@ -1,54 +1,53 @@
 package banking;
 
 public class Banking {
-    
-    public static void main(String[] args) {
-        
-       UserInputManager uim = new UserInputManager();
-       Bank bank = new Bank();
-              
-       String option1 = "[1] Add a new Client"; 
-       String option2 = "[2] Create a new Account";
-       String option3 = "[3] Make a Deposit";
-       String option4 = "[4] Make a Withdrawal";
-       String option5 = "[5] List Account Transactions";
-       String option6 = "[6] List Clients";
-       String option7 = "[7] List Client Accounts";
-       System.out.println("\n\n      Welcome to our bank \"Big Brains, Big Money\"\n\n=========================================================");       
-       System.out.printf("%n%-32s%-32s%n%-32s%-32s%n%-32s%-32s%n%-32s%n%n=========================================================%n%n",option1,option2,option3,option4,option5,option6,option7);
-       
-       int userOption = uim.retrieveUserOption();
-       int i = 2;
-       do{
-       switch (userOption){
-           
-           case 1:
-               System.out.println("You selected: Add a new Client");
-               bank.addClient(uim.retrieveClientInfo());
-               break;
-           case 2:
-               System.out.println("You selected: Create a new Account");
-               System.out.println(uim.retrieveClientId());
-               break;
-           case 3:
-               System.out.println("You selected: Make a Deposit");
-               break;
-           case 4:
-               System.out.println("You selected: Make a Withdrawal");
-               break;
-           case 5:
-               System.out.println("You selected: List Account Transactions");
-               break;
-            case 6:
-               System.out.println("You selected: List Clients");
-               break;
-            case 7:
-               System.out.println("You selected: List Client Accounts");
-               break;
-             }
-            userOption = uim.retrieveUserOption();
-        } while(i==2);
-       
-       } 
-    }
 
+    public static void main(String[] args) {
+        UserInputManager uim = new UserInputManager();
+        Bank bank = new Bank();
+
+        //System.out.print(uim.scanInput());
+        String option1 = "[1] Add a new Client";
+        String option2 = "[2] Create a new Account";
+        String option3 = "[3] Make a Deposit";
+        String option4 = "[4] Make a Withdrawal";
+        String option5 = "[5] List Account Transactions";
+        String option6 = "[6] List Clients";
+        String option7 = "[7] List Client Accounts";
+        System.out.println("\n\n      Welcome to our bank \"Big Brains, Big Money\"\n\n=========================================================");
+        System.out.printf("%n%-32s%-32s%n%-32s%-32s%n%-32s%-32s%n%-32s%n%n=========================================================%n%n", option1, option2, option3, option4, option5, option6, option7);
+
+        int userOption = uim.retrieveUserOption();
+
+        while (!uim.userInput.equals("stop")) {
+            switch (userOption) {
+
+                case 1:
+                    System.out.println("You selected: Add a new Client");
+                    bank.addClient(uim.retrieveClientInfo());
+                    break;
+                case 2:
+                    System.out.println("You selected: Create a new Account");
+                    System.out.println(uim.retrieveClientId());
+                    break;
+                case 3:
+                    System.out.println("You selected: Make a Deposit");
+                    break;
+                case 4:
+                    System.out.println("You selected: Make a Withdrawal");
+                    break;
+                case 5:
+                    System.out.println("You selected: List Account Transactions");
+                    break;
+                case 6:
+                    System.out.println("You selected: List Clients");
+                    break;
+                case 7:
+                    System.out.println("You selected: List Client Accounts");
+                    break;
+            }
+            userOption = uim.retrieveUserOption();
+        }
+
+    }
+}
--- Classes/UserInputManager.java
+++ Classes/UserInputManager.java
@@ -1,88 +1,114 @@
 package banking;
+
 import java.util.Scanner;
 
 public class UserInputManager {
-     Scanner scan = new Scanner(System.in);
-   // Client TestObject = new Client(retrieveClientId(),firstName,lastName,accountList);
-    int userOption;
 
-    int retrieveAccountNumber(){
+    Scanner scan = new Scanner(System.in);
+    String userInput;
+
+    //Dan-23nov This function allows for a general Scan, letting further functions check if it corresponds to the desired type, i.e. is "phallus" a valid userOption? 
+    String scanInput(String desiredType) {
+
+        boolean canBeConverted = false;
+
+        while (canBeConverted == false) {
+
+            userInput = scan.next();
+
+            System.out.println("\nInput: " + userInput);
+            //Dan- Stops the program when "stop" is input
+            if (userInput.equals("stop")) {
+                System.err.println("INPUT WAS 'stop' (STOPPING THE PROGRAM)");
+                System.exit(0);
+            } else {
+                //Dan- prevents crashes if wrong type is scanned
+                switch (desiredType) {
+
+                    case "integer":
+                        try {
+                            int i = Integer.parseInt(userInput);
+                            canBeConverted = true;
+                            return userInput;
+                        } catch (Exception e) {
+                            System.err.println("The input must be a whole number , please try again.\n");
+                            break;
+                        }
+
+                }
+            }
+        }
+        System.out.println("reached end. BAD");
+        return userInput;
+    }
+
+    //Dan-20nov: When I call this function, it asks me to do it twice.. Please fix. -Dan    I did it!!!! -frahan
+    int retrieveUserOption() {
+
+        int userOption = -1;
+
+        while (userOption == -1) {
+
+            System.out.println("Please enter the digit of one of the 7 options available: ");
+            userOption = Integer.parseInt(scanInput("integer"));
+            
+            if (userOption < 1 || userOption > 7) {
+                System.err.println(userOption + " is not an option. Please try again.");
+                userOption = -1;
+            }
+        }
+        return userOption;
+    }
+
+//Updates: Dan-nov22, I changed it to "nextLine()" because some
+    Client retrieveClientInfo() {
+
+        System.out.println("What is your first name?");
+        String firstName = scan.nextLine();                                                            // get variables firstName, lastName from Client??  
+        System.out.println("What is your last name?");
+        String lastName = scan.nextLine();
+        Client client = new Client(firstName, lastName);                                                             // needs 'firstName,lastName' constructor
+        return client;
+    }
+
+    int retrieveAccountNumber() {
 
         System.out.print("Please enter the account number of the desired account: ");
         int accountNum = scan.nextInt();
         return accountNum;
     }
-    Account retrieveAccountType(){
+
+    // THIS NEEDS TO BE REWORKED
+    Account retrieveAccountType() {
         Account test = new Account();
         System.out.println("Please enter the type of account desired (\"1\" for Checkings, \"2\" for Savings) ");
         int accountType = scan.nextInt();                                                       //default type is 3, meaning no type
         return test;
     }
-    int retrieveClientId(){
 
-        
+    int retrieveClientId() {
+
         System.out.print("Please enter your client id: ");
         int id = scan.nextInt();
         int clientLength = Bank.getClientList().size() + 1;
         int r = 1;
         for (int i = 0; i < r; i++) {
-             if(id <= clientLength && id > 0){}
-        else{
-            System.out.println("banking.UserInputManager.retrieveClientId()");
-            r++;
+            if (id <= clientLength && id > 0) {
+            } else {
+                System.out.println("banking.UserInputManager.retrieveClientId()");
+                r++;
+            }
         }
-        }
-     
-        return id;
-        
-    }
-    Client retrieveClientInfo(){
 
-         System.out.println("What is your first name?");
-        String firstName = scan.next();                                                            // get variables firstName, lastName from Client??  
-        System.out.println("What is your last name?");
-        String lastName = scan.next();
-        Client client = new Client(firstName,lastName);                                                             // needs 'firstName,lastName' constructor
-        return client;
+        return id;
+
     }
-    double retrieveTransactionAmount(){
+
+    double retrieveTransactionAmount() {
         System.out.print("Please enter the desired amount for the current transaction: ");
         retrieveTransactionAmount();
         double transAmount = scan.nextDouble();
         return transAmount;
     }
-    
-    //Code Review 20nov: When I call this function, it asks me to do it twice.. Please fix. -Dan    I did it!!!! -frahan
-    int retrieveUserOption(){
 
-        /*String userOption = scan.next();
-        for(int i = 1; i=<7; i++){
-    
-        } 
-        if(userOption.contains("1")){
-        
-        }else{
-            System.out.println("Please select the appropriate option");
-        }
-        
-        System.out.println(userOption);
-        */
-        
-        
-         int r = 1;
-            for (int i = 0; i < r; i++) {
-                System.out.println("Please enter the digit of one of the 7 options available: ");
-            userOption = scan.nextInt();
-
-            if(userOption <= 7 && userOption >= 1){                
-
-            }
-            else{
-                r++;
-                System.err.println("Not a valid option - Please try again!!!");
-
-            }
-        }
-        return userOption;
-    }
 }
