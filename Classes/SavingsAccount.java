53ed97c3edd6ce16f7e0f69fced141ed6b75d282
MariusAxente <57196700+MariusAxente@users.noreply.github.com>
2019-11-27 16:08:13 -0500

    Update SavingsAccount.java

--- Classes/SavingsAccount.java
+++ Classes/SavingsAccount.java
@@ -1,6 +1,6 @@
 package banking;
 
-public class SavingsAccount extends Account{
+public class SavingsAccount extends Account{  //Marius
 
     public SavingsAccount(Client owner, String type) {
         super(owner, type);
