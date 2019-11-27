5026407137eee1ba133ab82bf42b7c41c7a17443
MariusAxente <57196700+MariusAxente@users.noreply.github.com>
2019-11-27 16:08:36 -0500

    Update CheckingAccount.java

--- Classes/CheckingAccount.java
+++ Classes/CheckingAccount.java
@@ -1,6 +1,6 @@
 package banking;
 
-public class CheckingAccount extends Account{
+public class CheckingAccount extends Account{  //Marius
 
     public CheckingAccount(Client owner, String type) {
         super(owner, type);
