58ebcd8b85cbe6bd8e57f1d48c74db3fd0d7ba2c
FarhanShoaib <57242946+FarhanShoaib@users.noreply.github.com>
2019-11-28 11:13:01 -0500

    moved functions to Banking class

--- Classes/Banking.java
+++ Classes/Banking.java
@@ -1,5 +1,5 @@
 package banking;
-
+import java.util.Scanner;
 public class Banking {
 
     /* NOTES
@@ -33,7 +33,166 @@ public class Banking {
                 remember, the scanInput always returns a STRING, it only CHECKS if the user put in a correct value.
                 
     */
+    private static boolean valid; //Is the input usable by the calling method?
+    private static int inputLength;
+    private static int counter; //used for counting
+    private static String userInput;
+    private static String validInput; //Final and (if needed) transformed input.
+    private static String sample;   //used for substrings.
     
+    private static Scanner scan = new Scanner(System.in);
+    
+    
+     public static String scanInput(String desiredReturn) { //Daniel: When the console needs to retrieve information, this code will prevent unwanted inputs.
+        do {//Until the appropriate conditions are met, scanned values will not be returned.
+            userInput = scan.nextLine();
+            inputLength = userInput.length();
+            counter = 0;
+            valid = false;
+
+            switch (userInput) {
+
+                //Daniel
+                case "stop":
+                    System.err.println("INPUT WAS 'stop' (STOPPING THE PROGRAM)");
+                    System.exit(0);
+
+                //Daniel    
+                case "":
+                    System.err.println("Nothing happened.\n");
+                    break;
+
+                default:
+                    switch (desiredReturn) {
+
+                        //Daniel
+                        case "option":
+
+                            try {     //Only allow integers 1 to 7. 
+                                int i = Integer.parseInt(userInput);
+                                if (i <= 7 && i >= 1) {
+                                    valid = true;
+                                    validInput = userInput;
+                                } else {
+                                    valid = false;
+                                }
+                            } catch (Exception e) {
+                                valid = false;
+                            }
+                            if (valid == false) {
+                                System.err.println("Your input must correspond to one of the options [1,7]");
+                            }
+                            break;
+
+                        //Daniel    
+                        case "name":
+                            String possibleComponents[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-", " "};
+                            //Every letter must correspond to one in the array
+                            do {
+                                sample = userInput.substring(counter, counter + 1);
+                                valid = false;
+                                for (String pc : possibleComponents) {
+                                    if (sample.equals(pc)) {
+                                        valid = true;
+                                    }
+                                }
+                                if (valid == false) {
+                                    counter = inputLength;
+                                }
+                                counter++;
+                            } while (counter < inputLength);
+                            if (valid == true) {  //If each component has a match, capitalize and return.
+
+                                validInput = autoCapitalize(userInput);
+                            } else {
+                                System.err.println("Your name is not valid. Please use a-Z.\n");
+                            }
+                            break;
+
+                        //Daniel
+                        case "clientId":
+
+                            try {     //Only allow integers of existing ids.      
+                                int i = Integer.parseInt(userInput);
+                                if (i <= Bank.getClientList().size() && i >= 1) {
+                                    valid = true;
+                                    validInput = userInput;
+                                } else {
+                                    valid = false;
+                                }
+                            } catch (Exception e) {
+                                valid = false;
+                            }
+                            if (valid == false) {
+                                System.err.println("It appears that client [" + userInput + "] does not exist. Are they perhaps a ghost? Please try again.");
+                            }
+                            break;
+
+                        //Daniel   
+                        case "accountType":
+
+                            try {
+                                int i = Integer.parseInt(userInput);
+                                if (i <= 2 && i >= 1) {
+                                    valid = true;
+                                    validInput = userInput;
+                                } else {
+                                    valid = false;
+                                }
+                            } catch (Exception e) {
+                                valid = false;
+                            }
+                            if (valid == false) {
+                                System.err.println("[" + userInput + "] is not an option. Please select [1] or [2].");
+                            }
+                            break;
+
+                        case "money":
+
+                            System.out.println("HMMMMMMMM WE LOVE MONEYYYYYYYYYYYYYYYYYYYY");
+
+                            break;
+
+                    }
+                    break;
+            }
+        } while (valid == false);
+        return validInput;
+    }
+     public static String autoCapitalize(String initialString) {
+        //This code does two things: 1. Removes uneeded spaces and hypens 2. Capitalizes names (even ones made of two parts)
+        initialString = " " + initialString + " ";
+        String transformedString = "";
+        counter = 0;
+        while (counter <= initialString.length() - 1) {
+            sample = initialString.substring(counter, counter + 1);
+            if (sample.equals(" ") || sample.equals("-")) {
+                transformedString = transformedString + sample;
+                while (sample.equals(" ") || sample.equals("-")) {
+                    try {
+                        sample = initialString.substring(counter, counter + 1);
+                        counter++;
+                    } catch (Exception e) {
+                        counter++;
+                        sample = "";
+                    }
+                }
+                sample = sample.toUpperCase();
+                transformedString = transformedString + sample;
+            } else {
+                sample = (initialString.substring(counter, counter + 1)).toLowerCase();
+                transformedString = transformedString + sample;
+                counter++;
+            }
+        }
+        if (transformedString.substring(transformedString.length() - 1, transformedString.length()).equals(" ")) {
+            transformedString = transformedString.substring(0, transformedString.length() - 1);
+        }
+        if (transformedString.substring(0, 1).equals(" ")) {
+            transformedString = transformedString.substring(1, transformedString.length());
+        }
+        return transformedString;
+    }
 
     public static void main(String[] args) {
         
@@ -55,7 +214,12 @@ public class Banking {
                     break;
                     
                 case 3:
-                    bank.createTransaction("deposit");                   
+                    bank.createTransaction("deposit");    
+                    //@RR
+                    int clientId = 1;
+                    int accountId = 1;
+                    Account a = bank.getClientAccount(clientId, accountId);
+                    a.deposit(500.00);
                     break;
                     
                 case 4:
@@ -78,4 +242,7 @@ public class Banking {
         }
 
     }
+
+    
 }
+
